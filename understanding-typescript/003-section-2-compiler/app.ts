c57623b15cbad2a612cdb7ee11f0f6ba72e0c07d
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2022-10-22 10:28:39 +1100

    Complete section 4, start section 4

---
+++ understanding-typescript/003-section-2-compiler/app.ts
@@ -0,0 +1,10 @@
+// This file is showing possible error types on purpose!
+
+const unUsed = 'dog';
+
+function test(arg1: string, arg2: numer) {
+  if (arg1) return '1';
+  return arg1;
+}
+
+test('1');
