cc6ee7cd4dcd9f54c9d25cbb095662d2da28c55e
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-04-02 06:32:52 +1000

    Matches [Test]: Update initial test case to use Matches class

--- typescript-the-complete-developers-guide/11.reusable-code/01.sport-report/src/Matches.test.ts
+++ typescript-the-complete-developers-guide/11.reusable-code/01.sport-report/src/Matches.test.ts
@@ -1,7 +1,10 @@
+import { Matches } from './Matches';
+
 describe('Matches', () => {
   describe('Given a team name', () => {
     test('Then provide their total wins', () => {
-      expect(true).toBeTruthy();
+      const matchReporter = new Matches();
+      expect(matchReporter.totalWins()).toEqual(0);
     });
   });
 });
