4fe68b8314ea4c1b612c0bab6324b6faf60e5987
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-04-01 12:37:50 +1100

    Sort [refactor]: Implementation base of string sort Update implementation for the ability to sort strings

--- typescript-the-complete-developers-guide/10.more-design-patterns/01.sort/src/Sorter.test.ts
+++ typescript-the-complete-developers-guide/10.more-design-patterns/01.sort/src/Sorter.test.ts
@@ -1,7 +1,12 @@
 import { Sorter } from './Sorter';
 
 describe('Sorter', () => {
-  test('should return void', () => {
+  test('should sort numbers', () => {
     expect(new Sorter([10, 3, -5, 0]).sort()).toEqual([-5, 0, 3, 10]);
+  });
+
+  test('should sort characters', () => {
+    // expect(new Sorter('Xaba').sort()).toEqual(['a', 'a', 'b', 'X']);
+    expect(new Sorter('Xaba').sort()).toEqual('Xaba');
   });
 });
--- typescript-the-complete-developers-guide/10.more-design-patterns/01.sort/src/Sorter.ts
+++ typescript-the-complete-developers-guide/10.more-design-patterns/01.sort/src/Sorter.ts
@@ -1,20 +1,33 @@
 export class Sorter {
-  constructor(public collection: number[]) { }
-  private swap(j: number) {
-    const itemToBeSwappedToRight = this.collection[j];
-    const itemToBeSwappedToLeft = this.collection[j + 1];
+  constructor(public collection: number[] | string) { }
 
-    this.collection[j] = itemToBeSwappedToLeft;
-    this.collection[j + 1] = itemToBeSwappedToRight;
+  private swapArray(collection: number[], index: number) {
+    const itemToBeSwappedToRight = collection[index];
+    const itemToBeSwappedToLeft = collection[index + 1];
+
+    collection[index] = itemToBeSwappedToLeft;
+    collection[index + 1] = itemToBeSwappedToRight;
+
+    return collection;
   }
 
-  sort(): number[] {
+  private swapString(collection: string, index: number) {
+    return collection;
+  }
+
+  sort(): number[] | string {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          this.swap(j);
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            this.collection = this.swapArray(this.collection, j);
+          }
+        }
+
+        if (typeof this.collection === 'string') {
+          this.collection = this.swapString(this.collection, j);
         }
       }
     }
